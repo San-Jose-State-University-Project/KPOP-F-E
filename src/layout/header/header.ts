752f17a715c41c68af932d8e6a0eed0b783d9755
dohun08 <bemik624@gmail.com>
2025-05-16 01:05:25 +0900

    feat(#1) :: 헤더 완성

---
+++ src/layout/header/header.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export  const Header = styled.header`
+    width: 100%;
+    padding: 20px 120px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+`;
+export const Logo = styled.img`
+    width: 130px;
+`
+export const SearchBtn = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 15px;
+    background-color: ${({ theme }) => theme.colors.primary};
+    border-radius: 5px;
+    border: none;
+    color: white;
+    font-size: 14px;
+    gap: 10px;
+    cursor: pointer;
+    &:hover{
+        opacity: 0.7;
+        transition: 0.2s;
+    }
+`
+export const Nav = styled.nav`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    gap: 20px;
+    align-items: center;
+    justify-content: space-between;
+    & > p:hover{
+        cursor: pointer;
+        opacity: 0.8;
+        transition: 0.2s;
+    }
+`
--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,9 +1,19 @@
-
+import * as S from "./header.ts"
+import Search from "@/assets/search.svg"
+import Logo from "@/assets/logo.svg"
 
 export default function Header(){
     return(
-        <header>
-            <h1>Header</h1>
-        </header>
+        <S.Header>
+            <S.Logo src={Logo} alt={"logo"}/>
+            <S.Nav>
+                <p>Home</p>
+                <p>Trand</p>
+            </S.Nav>
+            <S.SearchBtn>
+                <img src={Search} alt={"search"} />
+                <p>Artist Search</p>
+            </S.SearchBtn>
+        </S.Header>
     )
 }
