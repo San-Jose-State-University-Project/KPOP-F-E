7c3f46a9a0f0fc2b771777310fcdf843ff79bc54
dohun08 <bemik624@gmail.com>
2025-05-16 19:18:26 +0900

    feat(#2) :: 랭킹페이지 퍼블리싱

--- src/components/ranking/style.ts
+++ src/components/ranking/style.ts
@@ -28,16 +28,71 @@ export const Button = styled.button`
 export const Main = styled.main`
     width: 100%;
     display: flex;
-    gap: 30px;
+    gap: 14px;
     padding: 0 30px;
     transition: 0.2s;
+    height: 80%;
+    flex-direction: column;
+    & > div{
+        display: flex;
+        align-items: center;
+    }
 `
-export const Info = styled.div`
+export const RankIcon = styled.div`
+    width: 5%;
+    & > img{
+        width: 40px;
+    }
+`
+interface Props {
+    isRank : boolean
+}
+export const Info = styled.div<Props>`
     display: flex;
     align-items: center;
     gap: 10px;
-    width: 20%;
+    justify-content: center;
+    width: 15%;
+    font-size: ${(props) => props.isRank ? "1.2rem" : "0.84rem"};
+    & > div{
+        text-align: center;
+    }
+    & > div > p{
+        color: ${({ theme }) => theme.colors.gray};
+    }
 `
-export const Bar = styled.div`
-    width: 80%;
+type ExtendedProps = Props & {
+    length: number;
+};
+
+export const Bar = styled.div<ExtendedProps>`
+    width: ${(props) => props.length * 8}%;
+    height: ${(props) => props.isRank ? "100" : "80"}%;
+    position: relative;
+    overflow: hidden;
+    background-color: #1D1D37;
+    border-top-right-radius: 20px;
+    border-bottom-right-radius: 20px;
+    & > section{
+        max-width: 200px;
+        overflow: hidden;
+        height: 100%;
+        position: absolute;
+        right: 0;
+        z-index: 4;
+    }
+    & > section > img{
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+    }
+`
+export const BarColor = styled.div`
+    width: 200px;
+    overflow: hidden;
+    height: 100%;
+    background: linear-gradient(270deg, rgba(23, 22, 38, 0.00) 50.33%, #1D1D37 100%);
+    position: absolute;
+    right: 0;
+    z-index: 10;
 `
