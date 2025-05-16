192fccde9d0e5eb40c82702e771eca6dd9a6ec74
dohun08 <bemik624@gmail.com>
2025-05-16 22:32:32 +0900

    feat(#4) :: 아티스트 검색 창 퍼블리싱 완료

--- src/pages/search/search.tsx
+++ src/pages/search/search.tsx
@@ -1,10 +1,53 @@
 import Layout from "../../layout";
-
+import styled from "styled-components";
+import SearchIcon from "@/assets/searchIcon.svg"
+import Result from "../../components/result";
 
 export default function Search(){
     return(
         <Layout>
-
+            <Container>
+                <h1>Search</h1>
+                <InputBox>
+                    <input type="text" placeholder="Enter artist name" />
+                    <div>
+                        <img src={SearchIcon} alt={"search"} />
+                    </div>
+                </InputBox>
+                <Result />
+            </Container>
         </Layout>
     )
 }
+
+export const Container = styled.main`
+    width: 100%;
+    height: 80vh;
+    display: flex;
+    padding: 0 20.44rem;
+    flex-direction: column;
+    gap: 10px;
+`
+export const InputBox = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    padding: 0.5rem 2rem;
+    border-radius: 1.25rem;
+    & > input{
+        width: 90%;
+        border: none;
+        font-size: 18px;
+        outline: none;
+    }
+    & > div{
+        width: 10%;
+        text-align: right;
+        cursor: pointer;
+        & > img{
+            width: 30px;
+        }
+    }
+`
