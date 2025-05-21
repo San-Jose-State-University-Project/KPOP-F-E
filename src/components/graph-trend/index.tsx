4bc705777471bb5e990b987d64f49fbd4091b7e9
dohun08 <bemik624@gmail.com>
2025-05-21 23:10:22 +0900

    feat(#27) :: 장르분포 퍼블리싱

--- src/components/graph-trend/index.tsx
+++ src/components/graph-trend/index.tsx
@@ -1,9 +1,62 @@
 import * as S from "./style.ts"
+import {useState} from "react";
 
 export default function GraphTrend() {
+    const data = Array.from({length : 18})
+    const genreKeys = [
+        "k-pop",
+        "art pop",
+        "pop",
+        "hip hop",
+        "west coast hip hop",
+        "k-rap",
+        "k-rock",
+        "k-ballad",
+        "bubblegum pop",
+        "j-pop",
+        "japanese indie",
+        "j-rock",
+        "r&b",
+        "anime",
+        "rap",
+        "britpop",
+        "madchester",
+        "rock"
+    ];
+    const [isHover, setIsHover] = useState(
+        Array.from({length : 18}).map(()=>false)
+    );
+    const handleMouseEnter = (idx : number) => {
+        const newIsHover = [...isHover];
+        newIsHover[idx] = true;
+        setIsHover(newIsHover);
+    }
     return (
         <S.Container>
+            <S.BarBox>
+                {data.map((_, idx)=>{
+                    return(
+                            <S.Bar
+                                key={idx}
+                                onMouseEnter={()=>handleMouseEnter(idx)}
+                                onMouseLeave={() => setIsHover(prev => prev.map(() => false))}
+                            >
+                                {isHover[idx] &&
+                                    <S.InfoBox>
+                                        <S.Info>{genreKeys[idx]}</S.Info>
+                                        <S.Info>Total : 3</S.Info>
+                                    </S.InfoBox>
+                                }
+                            </S.Bar>
+                        )
 
+                })}
+            </S.BarBox>
+            <S.TextBox>
+                {genreKeys.map((item)=>{
+                    return <S.Text>{item}</S.Text>
+                })}
+            </S.TextBox>
         </S.Container>
     )
 }
--- src/components/graph-trend/style.ts
+++ src/components/graph-trend/style.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
@@ -7,5 +7,80 @@ export const Container = styled.div`
     gap: 20px;
     padding: 20px 120px;
     height: 100%;
-    background: #161520;
+`
+const barAnimation = () => {
+    return keyframes`
+        0%{
+            height: 0;
+        }
+        100%{
+            height: 100%;
+        }
+    `
+}
+export const BarBox = styled.div`
+    width: 100%;
+    align-items: end;
+    height: 80%;
+    padding: 0 20px;
+    display: grid;
+    grid-template-columns: repeat(18, 1fr);
+    gap: 20px;
+    border-bottom: 1px solid white;
+`
+export const Bar = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+    animation: ${barAnimation} 1.5s cubic-bezier(0.1, 0, 0.05, 1) forwards;
+    background-color: #1D1D37;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    transition: 0.4s;
+    &:hover{
+        background: #3C3C6B;
+    }
+`
+
+export const TextBox = styled.div`
+    width: 100%;
+    height: 3%;
+    display: grid;
+    padding: 0 20px;
+    grid-template-columns: repeat(18, 1fr);
+    gap: 20px;
+`
+export const Text = styled.p`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: white;
+    font-weight: 600;
+    font-size: 0.6rem;
+    transition: 0.2s;
+`
+export const InfoBox = styled.div`
+    width: max-content;
+    border-radius: 10px;
+    position: absolute;
+    top: -8%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+    color: white;
+    font-weight: 600;
+    font-size: 0.8rem;
+    padding: 15px 20px;
+    background: #2B2B2B;
+`
+export const Info = styled.p`
 `
